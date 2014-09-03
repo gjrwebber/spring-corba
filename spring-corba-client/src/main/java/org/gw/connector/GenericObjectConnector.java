9979ecec20f7e371c85088dcc6d65bf518018924
Graham Webber <gjrwebber@gmail.com>
2014-09-04 09:47:58 +1000

    GenericObjectConnector is now lazy by default

--- spring-corba-client/src/main/java/org/gw/connector/GenericObjectConnector.java
+++ spring-corba-client/src/main/java/org/gw/connector/GenericObjectConnector.java
@@ -52,7 +52,7 @@ public abstract class GenericObjectConnector<C extends Object>
      * If set to true, then the object is not connected until the first method
      * call.
      */
-    private boolean lazy = false;
+    private boolean lazy = true;
 
     /**
      * If set to false and the connected object is found to be disconnected on a
